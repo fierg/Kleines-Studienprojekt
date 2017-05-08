007de6a7a96608fe608766ce25a85f7586485fc8
fierg <sven.fiergolla@gmail.com>
2017-05-08 19:19:05 +0200

    Create Aufgabe2.c

---
+++ Aufgabe2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int utf8_strlen(char *s) 
+{
+  int arrayLength = 0, stringLength = 0;
+  while (s[arrayLength]) 
+  {
+    if ((s[arrayLength] & 0xc0) != 0x80) stringLength++;
+    arrayLength++;
+  }
+  return stringLength;
+}
+
+main(){
+    char *a = "hallo";
+  	char *b = "Längentest";
+	
+    int length1 = strlen(a);
+    int length2 = strlen(b);
+    
+    printf("strlen() lentgh of %s = %d \n", a,length1);
+  	printf("unicode-length of %s = %d \n",a, utf8_strlen(a));
+
+    printf("strlen() lentgh of %s = %d \n", b,length2);
+	  printf("unicode-length of %s = %d \n",b, utf8_strlen(b));
+	
+}
