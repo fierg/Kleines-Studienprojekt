9e64f84f317050fe2791f3c995241161eeaf88af
fierg <sven.fiergolla@gmail.com>
2017-06-25 18:26:29 +0200

    progress with the hashmap

--- Aufgabe3.c
+++ Aufgabe3.c
@@ -3,83 +3,68 @@
 #include <stdlib.h>
 #include <search.h>
 
-#define MAX_HASH_SIZE
+static const int MAX_HASH_SIZE = 5000;
 
 int findSubstring(char *str, char *substr) {
-     int i = 0;
-     int d = 0;
-	 /* setze Index i auf jedes Zeichen von str und prüfe, ob von i aus substr gefunden wurde */
-	 for (i = 0; i < strlen(str) - strlen(substr); i++) {
-		 int found = 1; 
-		 for (d = 0; d < strlen(substr); d++) {
-			 if (str[i + d] != substr[d]) {
-				 /* sobald ein character nicht übereinstimmt, setze found = 0 und suche ab dem nächsten Zeichen in str*/
-				 found = 0; 
-				 break;
-			 }
-		 }
-		 if (found == 1) {
-			 return i;
-		 }
-	 }
-	 return -1;
-} 
-
-char** getTitleTokens(char* key) {
-        char ** sub_str = malloc(20 * sizeof(char*));
-        for (int i =0 ; i < 10; ++i)
-            sub_str[i] = malloc(30 * sizeof(char));
-
-        int i = 0;
-        char *p = strtok (key, " ");
-
-        while (p != NULL)
-        {
-                sub_str[i++] = p;
-                p = strtok (NULL, "/");
-        }
-        return sub_str;
+	int i = 0;
+	int d = 0;
+	/* setze Index i auf jedes Zeichen von str und prüfe, ob von i aus substr gefunden wurde */
+	for (i = 0; i < strlen(str) - strlen(substr); i++) {
+		int found = 1;
+		for (d = 0; d < strlen(substr); d++) {
+			if (str[i + d] != substr[d]) {
+				/* sobald ein character nicht übereinstimmt, setze found = 0 und suche ab dem nächsten Zeichen in str*/
+				found = 0;
+				break;
+			}
+		}
+		if (found == 1) {
+			return i;
+		}
+	}
+	return -1;
 }
 
+int main() {
+	FILE *fp;
+	char line[1024];
+	int debug = 0;
 
-
-int main(){
-   FILE *fp;
-   char line[1024];
-   int debug = 0;
-
-   /* öffne dblp.xml zum lesen */
-   fp = fopen("dblp_smallTest.xml" , "r");
-   if(fp == NULL){
-      perror("Error opening file");
-      return(-1);
+	/* öffne dblp.xml zum lesen */
+	fp = fopen("dblp_smallTest.xml", "r");
+	if (fp == NULL) {
+		perror("Error opening file");
+		return (-1);
 	}
 	/* TODO: Geeignete Datenstruktur zum speichern von den Keys und den zugehörigen Titeln anlegen */
-   ENTRY titleToken;
-   ENTRY *searchTitelToken;
-   (void) hcreate(MAX_HASH_SIZE);
-   /* validArticle wird auf 1 gestzt, sobald ein key="x/y/z" Attribut gefunden wurde.
-	  Ist validArticle == 1, so wird bei jeder neuen line statt nach einem "<article" nach einem "<title>" gesucht */
+	ENTRY titleToken;
+	ENTRY *searchTitelToken;
+	(void) hcreate(MAX_HASH_SIZE);
+	/* validArticle wird auf 1 gestzt, sobald ein key="x/y/z" Attribut gefunden wurde.
+	 Ist validArticle == 1, so wird bei jeder neuen line statt nach einem "<article" nach einem "<title>" gesucht */
 	int validArticle = 0;
-	
+
 	char *key;
 	char *title;
-	
-   /* lies Zeile für Zeile */
-   while( fgets (line, 1024, fp) != NULL ){
-		if(debug)printf("line: %s", line);
-		
-		if(!validArticle){
+
+	/* lies Zeile für Zeile */
+	while (fgets(line, 1024, fp) != NULL) {
+		if (debug)
+			printf("line: %s", line);
+
+		if (!validArticle) {
 			/* check for the beginning of an <article> Tag */
 			if (strstr(line, "<article") != NULL) {
-				if(debug)printf("line enthaelt article");
-				
+				if (debug)
+					printf("line enthaelt article");
+
 				/* <article wurde gefunden, teste ob es sich um ein journal handelt */
-				if(strstr(line, "key=\"journals/") != NULL){
+				if (strstr(line, "key=\"journals/") != NULL) {
 					/* finde den index von 'key="' und erhöhe ihn um 5, um die 5 buchstaben key=" zu "überspringen", danach um 9, um journals/ zu überspringen */
 					char *substr = "key=\"";
 					int index = findSubstring(line, substr) + 5 + 9;
-					if(debug)printf(", index after key=\"journals/: %d\n", index);
+					if (debug)
+						printf(", index after key=\"journals/: %d\n", index);
 
 					/* finde den index vom end-tag "> */
 					substr = "\">";
@@ -92,7 +77,7 @@ int main(){
 					line[endIndex] = '\0';
 
 					/* überprüfe ob key 3 Teile enthält */
-					if(findSubstring(key, "/") == -1){
+					if (findSubstring(key, "/") == -1) {
 						/* no valid key */
 						break;
 					} else {
@@ -106,48 +91,57 @@ int main(){
 					}
 				}
 			}
-		}else {
-			if(strstr(line, "<title>") != NULL){
-				if(debug)printf("title found\n");
+		} else {
+			if (strstr(line, "<title>") != NULL) {
+				if (debug)
+					printf("title found\n");
 				/* wurde <title> gefunden, so extrahiere den Titel bis </title> */
-				
+
 				/* finde den index vom </title> tag */
 				char *substr = "</title>";
 				int index = findSubstring(line, substr);
-				
+
 				/* setze das Ende des Strings auf den letzten Char vorm < */
 				line[index] = '\0';
-				
+
 				/* lasse *title auf den Anfang des Titels zeigen */
-				title = &line[7];
+				title = &line[11];
 				printf("title: %s\n", title);
-				
+
 				validArticle = 0;
 				/* setze validArticle wieder auf 0, damit nach "<article" gesucht wird */
-				
-				
+
 				/* Ab hier enthält Key den gesamten Artikelstring x/y/z und title den zugehörigen Titel */
 				/* tokenize: */
 				char *t = strtok(title, " .,;:<>/\\+*~#'^°!\"§$%&(){}[]?`´|-_");
-				while(t != NULL) {
+				while (t != NULL) {
 					printf("token: %s\n", t);
-					
+
 					/* TODO: Speichere die Wörter und ordne ihn dem letzten key zu */
-					
+
 					t = strtok(NULL, " .,;:<>/\\+*~#'^°!\"§$%&(){}[]?`´|-_");
 					titleToken.key = t;
-					titleToken.data = 1;
-					(void) hsearch (titleToken, ENTER);
+					searchTitelToken = hsearch(titleToken, FIND);
+					if (searchTitelToken == NULL) {
+						titleToken.data = (void *) 1;
+						searchTitelToken = hsearch(titleToken, ENTER);
+						if(searchTitelToken== NULL){
+							fprintf(stderr,"entry failed");
+						}
+					} else {
+					//	searchTitelToken.data = (void*)((int) searchTitelToken->data) +1;
+					}
+					printf("hashed token key: %s , count: %d\n", searchTitelToken->key,(int)searchTitelToken->data);
 				}
-			}		
+			}
 		}
-		
+
 	}
-   fclose(fp);
-   
-   /* TODO: Überprüfe für jeden key, ob die zugehörige Zeitschrift >500 Titel enthält.*/
-   /* TODO: Zerlege Titel aller großen Zeitschriften und erstelle Wort-Statistik */
-   /* TODO: Erstelle geeignete Statistik zum Erstellen von Word-Clouds für die ersten x (vllt. 30?) Worte */
-   
-   return(0);
+	fclose(fp);
+
+	/* TODO: Überprüfe für jeden key, ob die zugehörige Zeitschrift >500 Titel enthält.*/
+	/* TODO: Zerlege Titel aller großen Zeitschriften und erstelle Wort-Statistik */
+	/* TODO: Erstelle geeignete Statistik zum Erstellen von Word-Clouds für die ersten x (vllt. 30?) Worte */
+
+	return (0);
 }
